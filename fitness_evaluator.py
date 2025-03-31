2ca43a176c6267a09af3333b3689ec58530f8b12
Anonymous <anon@nowhere.com>
2025-03-30 23:02:17 -0600

    Move fitness measurement inside GeneticAlgorithm

--- fitness_evaluator.py
+++
@@ -1,6 +0,0 @@
-"""
-Fitness evaluation functionality used by the genetic algorithm.
-It will receive an approximation image and a target image, then measure the
-RGB distance between the two. MSE to start, but eventually will account for
-curved color space.
-"""
