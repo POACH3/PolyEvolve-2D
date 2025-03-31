466c6b4327ad77550a284bcb17a1134254731e7c
Anonymous <anon@nowhere.com>
2025-03-30 21:23:01 -0600

    Implement Population class

--- Population.py
+++ Population.py
@@ -1,3 +1,26 @@
 """
-A collection of Individuals, that represents an evolutional generation.
+population.py
+
+A Population represents a generation of candidate solutions (collections
+of genes) that approximate a target image.
+
+NOTES:
+For simplicity, initially the following will be fixed:
+    - generation size: 1000
+
 """
+
+from Individual import Individual
+
+class Population:
+    """
+    Represents a generation defined by a set Individuals.
+    """
+
+    def __init__(self, max_dims):
+        self.generation_size = 1000
+        self.individuals = []
+
+        for i in range(self.generation_size):
+            individual = Individual(max_dims)
+            self.individuals.append(individual)
