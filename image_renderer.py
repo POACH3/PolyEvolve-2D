01ef1d029946c8207760cc681940d2a099994031
Anonymous <anon@nowhere.com>
2025-03-30 21:49:59 -0600

    Start of ImageRenderer class implementation

--- image_renderer.py
+++ image_renderer.py
@@ -1,3 +1,29 @@
 """
-Responsible for image I/O and display.
+image_renderer.py
+
+Used by genetic_algorithm.py to create candidate images.
+Also responsible for image I/O and display.
 """
+
+from PIL import Image
+
+class ImageRenderer:
+    def load_image(self, path):
+        """Load an image from the given path using Pillow."""
+        return Image.open(path)
+
+    def save_image(self, image, path):
+        """Save the image to the specified path."""
+        image.save(path)
+
+    def show_image(self, image):
+        """
+        Display the image.
+
+        Parameters:
+            image (Image)
+
+        Returns:
+
+        """
+        return image.show()
