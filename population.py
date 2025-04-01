bc71495450621e26289880fc51a6b7e5823da362
POACH3 <140459864+POACH3@users.noreply.github.com>
2025-03-31 21:09:49 -0600

    Fix random_individuals

--- population.py
+++ population.py
@@ -18,14 +18,30 @@ class Population:
     Represents a generation defined by a set Individuals.
     """
 
-    def __init__(self, max_dims):
-        self.generation_size = 1000
+    def __init__(self, individual_size, population_size=1000, individuals=None):
+        self.population_size = population_size
         self.population_fitness_score = -1 # average fitness
+        self.individual_size = individual_size
 
-        self.individuals = []
-        for i in range(self.generation_size):
-            individual = Individual(max_dims)
-            self.individuals.append(individual)
+        self.individuals = individuals if individuals else self.random_individuals()
+
+    def random_individuals(self):
+        """
+        Generates a random set of individuals.
+        """
+        new_individuals = []
+
+        for i in range(self.population_size):
+            individual = Individual(self.individual_size)
+            new_individuals.append(individual)
+
+        return new_individuals
 
     def set_fitness(self, average_fitness):
         self.population_fitness_score = average_fitness
+
+    def order_by_fitness(self):
+        """
+        Orders the population by individual fitness score (descending).
+        """
+        self.individuals.sort(key=lambda individual: individual.fitness, reverse=True)
