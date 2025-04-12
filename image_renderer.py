4859866b92d6c79a5973fa75082b75c400af715b
POACH3 <140459864+POACH3@users.noreply.github.com>
2025-04-12 15:57:53 -0600

    Change how create_image renders an individual

--- image_renderer.py
+++ image_renderer.py
@@ -33,19 +33,37 @@ class ImageRenderer:
         """
         Create an image from the given individual using Pillow.
 
+        An image is created for each gene and then merged to image for
+        the individual.
+
         Parameters:
             individual (Individual): The individual to be rendered.
 
         Returns:
             (Image): The rendered Pillow Image.
         """
-        image = Image.new("RGBA", individual.size, (0, 0, 0, 255)) # black, opaque background
-        draw = ImageDraw.Draw(image, "RGBA")
+        image_individual = Image.new("RGBA", individual.size, (0, 0, 0, 255)) # black, opaque background
+        #image_individual = Image.new("RGBA", individual.size, (255, 255, 255, 0))  # white, transparent background
+        #draw = ImageDraw.Draw(image_individual, "RGBA")
 
         for polygon in individual.genome:
+            image_gene = Image.new("RGBA", individual.size, (255, 255, 255, 0))  # white, transparent background
+            draw = ImageDraw.Draw(image_gene, "RGBA")
             draw.polygon(polygon.vertices, fill=polygon.color)
 
-        return image
+            # stack and blend the images
+            image_individual = Image.alpha_composite(image_individual, image_gene)
+
+        return image_individual
 
     def convert_image(self, image):
         return image.convert("RGBA")
+
+    def test(self):
+        image = Image.new("RGBA", (200, 200), (255, 255, 255, 255))
+        draw = ImageDraw.Draw(image, "RGBA")
+        # Draw green triangle at 50% opacity
+        draw.polygon([(0, 50), (120, 20), (100, 200)], fill=(0, 255, 0, 30))
+        # Draw red triangle on top at 50% opacity
+        draw.polygon([(50, 50), (150, 50), (100, 150)], fill=(255, 0, 0, 0))
+        image.save("output.png")
